f47c4b38dbc7840a99e47eeef745849f88b35f4f
Jamina <Jaminam@metropolia.fi>
2026-02-08 11:21:35 +0200

    Pinta-ala

--- Tehtävät/Viikko1.1.py
+++ Tehtävät/Viikko1.1.py
@@ -6,5 +6,14 @@ sade = float(sade_str)
 pinta_ala = math.pi * sade ** 2
 print(pinta_ala)
 
+Kanta = input("Anna kanta:")
+Kanta = float(Kanta)
+Korkeus = input("Anna Korkeus:")
+Korkeus = float(Korkeus)
+Piiri = 2*Kanta*Korkeus
+Pinta_ala = Kanta*Korkeus
+print("pinta_ala",Pinta_ala)
+print("piiri",Piiri)
+
 
 
