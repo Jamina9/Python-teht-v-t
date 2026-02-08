8969e3c0591da1651375fd8524ba7fbe954af3d6
Jamina <Jaminam@metropolia.fi>
2026-02-08 11:57:15 +0200

    Moduuli 2 tehtava 6

---
+++ Tehtävät/Viikko1.2..py
@@ -0,0 +1,12 @@
+import random
+n1 = random.randint(0,9)
+n2 = random.randint(0,9)
+n3 = random.randint(0,9)
+print("Kolmenumeroinen koodi")
+print(n1, n2, n3)
+m1 = random.randint(1,6)
+m2 = random.randint(1,6)
+m3 = random.randint(1,6)
+m4 = random.randint(1,6)
+print("Nelinumeroinen koodi")
+print(m1, m2, m3, m4)
