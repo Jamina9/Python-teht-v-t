c0bca7fc5b392c3962300591e7708278e2c65ec1
Jamina <Jaminam@metropolia.fi>
2026-03-25 18:04:16 +0200

    MODUULI 6.2 TEHTÄVÄ 3

---
+++ Moduuli 6.3.py
@@ -0,0 +1,14 @@
+def laske_summa(lista):
+    summa = 0
+
+    for luku in lista:
+        summa = summa + luku
+
+    return summa
+
+luvut = [1,2,3,4,5]
+tulos = laske_summa(luvut)
+
+print(tulos)
+
+
