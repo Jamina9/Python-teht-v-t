e5e60a2fcf7cf4b19b3aee4c544169d3830b9df9
Jamina <Jaminam@metropolia.fi>
2026-03-26 16:36:50 +0200

    KERTAUS 2 TEHTÄVÄ 4

---
+++ KERTAUS 2.3.py
@@ -0,0 +1,10 @@
+def kuusi(koko):
+    print("Tämä on kuusi!")
+
+    for i in range(1, koko + 1):
+        print(" " * (koko - i) + "*" * (2 * i -1))
+
+    print(" " * (koko-1)+ "*")
+
+kuusi(14)
+
